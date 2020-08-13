360760018e308fe45218978c0ddaf91454e21109
Krishna <kupadhyaya@techracers.io>
2020-08-13 13:06:43 +0530

    fix miner SC user-info data issue

--- cmd/minersc.go
+++ cmd/minersc.go
@@ -291,22 +291,26 @@ var minerscUserInfo = &cobra.Command{
 		}
 
 		var total common.Balance
-		for _, pools := range info.Pools {
-			for _, pool := range pools {
-				total += pool.Balance
+		for _, nodes := range info.Pools {
+			for _, pools := range nodes {
+				for _, pool := range pools {
+					total += pool.Balance
+				}
 			}
 		}
 
-		for nit, pools := range info.Pools {
-			fmt.Println("- node:", nit)
-			for _, pool := range pools {
-				fmt.Println("  - pool_id:       ", pool.ID)
-				fmt.Println("    balance:       ", pool.Balance)
-				fmt.Println("    interests paid:", pool.InterestPaid)
-				fmt.Println("    rewards paid:  ", pool.RewardPaid)
-				fmt.Println("    status:        ", strings.ToLower(pool.Status))
-				fmt.Println("    stake %:       ",
-					float64(pool.Balance)/float64(total)*100.0, "%")
+		for key, nodes := range info.Pools {
+			for nit, pools := range nodes {
+				fmt.Println("- node:", nit+" ("+key+")")
+				for _, pool := range pools {
+					fmt.Println("  - pool_id:       ", pool.ID)
+					fmt.Println("    balance:       ", pool.Balance)
+					fmt.Println("    interests paid:", pool.InterestPaid)
+					fmt.Println("    rewards paid:  ", pool.RewardPaid)
+					fmt.Println("    status:        ", strings.ToLower(pool.Status))
+					fmt.Println("    stake %:       ",
+						float64(pool.Balance)/float64(total)*100.0, "%")
+				}
 			}
 		}
 	},
