c05644e38b610f29804cc6758f8ea16414821f80
Alex Guo <chessnut@outlook.com>
2021-02-20 20:11:48 -0800

    Revert "zcncore: add debug info, print the JSON sent during submitTxn"
    This reverts commit f2854c9ab44d8cb58bd548c5beef86bc2032db56.

--- cmd/common.go
+++ cmd/common.go
@@ -1,129 +1,121 @@
 package cmd
 
 import (
-  "fmt"
-  "os"
-  "sync"
+	"fmt"
+	"os"
+	"sync"
 
-  "github.com/0chain/gosdk/zcncore"
-  "gopkg.in/cheggaaa/pb.v1"
+	"github.com/0chain/gosdk/zcncore"
+	"gopkg.in/cheggaaa/pb.v1"
 )
 
 type StatusBar struct {
-  b  *pb.ProgressBar
-  wg *sync.WaitGroup
+	b  *pb.ProgressBar
+	wg *sync.WaitGroup
 }
 
 type ZCNStatus struct {
-  walletString string
-  wg           *sync.WaitGroup
-  success      bool
-  errMsg       string
-  balance      int64
-  wallets      []string
-  clientID     string
+	walletString string
+	wg           *sync.WaitGroup
+	success      bool
+	errMsg       string
+	balance      int64
+	wallets      []string
+	clientID     string
 }
 
 func NewZCNStatus() (zcns *ZCNStatus) {
-  return &ZCNStatus{wg: new(sync.WaitGroup)}
+	return &ZCNStatus{wg: new(sync.WaitGroup)}
 }
 
 func (zcns *ZCNStatus) Begin() { zcns.wg.Add(1) }
 func (zcns *ZCNStatus) Wait()  { zcns.wg.Wait() }
 
 func (zcn *ZCNStatus) OnBalanceAvailable(status int, value int64, info string) {
-  defer zcn.wg.Done()
-  if status == zcncore.StatusSuccess {
-    zcn.success = true
-  } else {
-    zcn.success = false
-  }
-  zcn.balance = value
+	defer zcn.wg.Done()
+	if status == zcncore.StatusSuccess {
+		zcn.success = true
+	} else {
+		zcn.success = false
+	}
+	zcn.balance = value
 }
 
 func (zcn *ZCNStatus) OnTransactionComplete(t *zcncore.Transaction, status int) {
-  defer zcn.wg.Done()
-  if status == zcncore.StatusSuccess {
-    zcn.success = true
-  } else {
-    zcn.errMsg = t.GetTransactionError()
-    fmt.Println("aa1", zcn.errMsg)
-  }
-  // fmt.Println("Txn Hash:", t.GetTransactionHash())
+	defer zcn.wg.Done()
+	if status == zcncore.StatusSuccess {
+		zcn.success = true
+	} else {
+		zcn.errMsg = t.GetTransactionError()
+	}
+	// fmt.Println("Txn Hash:", t.GetTransactionHash())
 }
 
 func (zcn *ZCNStatus) OnVerifyComplete(t *zcncore.Transaction, status int) {
-  defer zcn.wg.Done()
-  if status == zcncore.StatusSuccess {
-    zcn.success = true
-  } else {
-    zcn.errMsg = t.GetVerifyError()
-    fmt.Println("aa2", zcn.errMsg)
-  }
-  // fmt.Println(t.GetVerifyOutput())
+	defer zcn.wg.Done()
+	if status == zcncore.StatusSuccess {
+		zcn.success = true
+	} else {
+		zcn.errMsg = t.GetVerifyError()
+	}
+	// fmt.Println(t.GetVerifyOutput())
 }
 
 func (zcn *ZCNStatus) OnAuthComplete(t *zcncore.Transaction, status int) {
-  fmt.Println("Authorization complete on zauth.", status)
+	fmt.Println("Authorization complete on zauth.", status)
 }
 
 func (zcn *ZCNStatus) OnWalletCreateComplete(status int, wallet string, err string) {
-  defer zcn.wg.Done()
-  if status != zcncore.StatusSuccess {
-    zcn.success = false
-    zcn.errMsg = err
-    fmt.Println("aa3", zcn.errMsg)
-    zcn.walletString = ""
-    return
-  }
-  zcn.success = true
-  zcn.errMsg = ""
-  fmt.Println("aa4", zcn.errMsg)
-  zcn.walletString = wallet
-  return
+	defer zcn.wg.Done()
+	if status != zcncore.StatusSuccess {
+		zcn.success = false
+		zcn.errMsg = err
+		zcn.walletString = ""
+		return
+	}
+	zcn.success = true
+	zcn.errMsg = ""
+	zcn.walletString = wallet
+	return
 }
 
 func (zcn *ZCNStatus) OnInfoAvailable(Op int, status int, config string, err string) {
-  defer zcn.wg.Done()
-  if status != zcncore.StatusSuccess {
-    zcn.success = false
-    zcn.errMsg = err
-    fmt.Println("aa5", zcn.errMsg)
-    return
-  }
-  zcn.success = true
-  zcn.errMsg = config
-  fmt.Println("aa6", zcn.errMsg)
+	defer zcn.wg.Done()
+	if status != zcncore.StatusSuccess {
+		zcn.success = false
+		zcn.errMsg = err
+		return
+	}
+	zcn.success = true
+	zcn.errMsg = config
 }
 
 func (zcn *ZCNStatus) OnSetupComplete(status int, err string) {
-  defer zcn.wg.Done()
+	defer zcn.wg.Done()
 }
 
 func (zcn *ZCNStatus) OnAuthorizeSendComplete(status int, toClienID string, val int64, desc string, creationDate int64, signature string) {
-  defer zcn.wg.Done()
-  fmt.Println("Status:", status)
-  fmt.Println("Timestamp:", creationDate)
-  fmt.Println("Signature:", signature)
+	defer zcn.wg.Done()
+	fmt.Println("Status:", status)
+	fmt.Println("Timestamp:", creationDate)
+	fmt.Println("Signature:", signature)
 }
 
 //OnVoteComplete callback when a multisig vote is completed
 func (zcn *ZCNStatus) OnVoteComplete(status int, proposal string, err string) {
-  defer zcn.wg.Done()
-  if status != zcncore.StatusSuccess {
-    zcn.success = false
-    zcn.errMsg = err
-    fmt.Println("aa7", zcn.errMsg)
-    zcn.walletString = ""
-    return
-  }
-  zcn.success = true
-  zcn.errMsg = ""
-  fmt.Println("aa8", zcn.errMsg)
-  zcn.walletString = proposal
+	defer zcn.wg.Done()
+	if status != zcncore.StatusSuccess {
+		zcn.success = false
+		zcn.errMsg = err
+		zcn.walletString = ""
+		return
+	}
+	zcn.success = true
+	zcn.errMsg = ""
+	zcn.walletString = proposal
 }
 
 func ExitWithError(v ...interface{}) {
-  fmt.Fprintln(os.Stderr, v...)
-  os.Exit(1)
+	fmt.Fprintln(os.Stderr, v...)
+	os.Exit(1)
 }
--- cmd/readpool.go
+++ cmd/readpool.go
@@ -7,51 +7,36 @@ import (
 	"github.com/0chain/gosdk/zcncore"
 )
 
-import "fmt"
-
 func createReadPool() (err error) {
-  fmt.Println("d1 a")
-  var (
-    txn       zcncore.TransactionScheme
-    wg        sync.WaitGroup
-    statusBar = &ZCNStatus{wg: &wg}
-  )
+	var (
+		txn       zcncore.TransactionScheme
+		wg        sync.WaitGroup
+		statusBar = &ZCNStatus{wg: &wg}
+	)
 
-  fmt.Println("d2.1", statusBar.errMsg)
+	if txn, err = zcncore.NewTransaction(statusBar, 0); err != nil {
+		return
+	}
 
-  if txn, err = zcncore.NewTransaction(statusBar, 0); err != nil {
-    fmt.Println("d1 b")
-    return
-  }
+	wg.Add(1)
+	if err = txn.CreateReadPool(0); err != nil {
+		return
+	}
+	wg.Wait()
 
-  fmt.Println("d2.2", statusBar.errMsg)
+	if statusBar.success {
+		statusBar.success = false
 
-  wg.Add(1)
-  fmt.Println("d2.2.1", statusBar.errMsg)
-  if err = txn.CreateReadPool(0); err != nil {
-    fmt.Println("d1 c")
-    return
-  }
-  fmt.Println("d2.2.2", statusBar.errMsg)
-  wg.Wait()
+		wg.Add(1)
+		if err = txn.Verify(); err != nil {
+			return
+		}
+		wg.Wait()
 
-  fmt.Println("d2.3", statusBar.errMsg)
-  if statusBar.success {
-    statusBar.success = false
+		if statusBar.success {
+			return // nil
+		}
+	}
 
-    wg.Add(1)
-    if err = txn.Verify(); err != nil {
-      fmt.Println("d1 d")
-      return
-    }
-    wg.Wait()
-
-    if statusBar.success {
-      fmt.Println("d1 e")
-      return // nil
-    }
-  }
-
-  fmt.Println("d1 f", statusBar.errMsg)
-  return errors.New(statusBar.errMsg)
+	return errors.New(statusBar.errMsg)
 }
