5581a3fb76ea3ed7272b2bc921eddc4df8b3c88f
DmitryD <dima.dyachkov@gmail.com>
2021-11-28 00:39:32 +0300

    Refactored the command into DRY code

--- cmd/bridge-verify.go
+++ cmd/bridge-verify.go
@@ -2,80 +2,36 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/0chain/gosdk/core/conf"
 	"github.com/0chain/gosdk/zcnbridge"
-	"github.com/spf13/cobra"
-	"path"
-	"path/filepath"
-	"strings"
 	"time"
 )
 
-var verifyEthereumTrxCmd = &cobra.Command{
-	Use:   "bridge-verify",
-	Short: "verify ethereum transaction ",
-	Long: `verify transaction.
-	        <hash>`,
-	Args: cobra.MinimumNArgs(0),
-	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			logPath    = "logs"
-			configFile string
-			configDir  string
-			hash       string
-			err        error
-		)
+const (
+	USE   = "bridge-verify"
+	SHORT = "verify ethereum transaction "
+	LONG  = `verify transaction.
+	        <hash>`
+)
 
-		fflags := cmd.Flags()
+func ConfirmEthereumTransaction(hash string) {
+	status, err := zcnbridge.ConfirmEthereumTransaction(hash, 5, time.Second)
+	if err != nil {
+		ExitWithError(err)
+	}
 
-		if !fflags.Changed("hash") {
-			ExitWithError("Error: hash flag is missing")
-		}
-		if !fflags.Changed("file") {
-			ExitWithError("Error: file flag is missing")
-		}
-
-		configFlag, err := fflags.GetString("file")
-		if err != nil {
-			ExitWithError(err)
-		}
-
-		hash, err = fflags.GetString("hash")
-		if err != nil {
-			ExitWithError(err)
-		}
-
-		configDir, configFile = filepath.Split(configFlag)
-		configFile = strings.TrimSuffix(configFile, path.Ext(configFlag))
-
-		clientConfig, _ := conf.GetClientConfig()
-		if clientConfig.EthereumNode == "" {
-			ExitWithError("ethereum_node_url must be setup in config")
-		}
-
-		bridge := zcnbridge.SetupBridge(configDir, configFile, false, logPath)
-		bridge.SetupEthereumWallet()
-
-		status, err := zcnbridge.ConfirmEthereumTransaction(hash, 5, time.Second)
-		if err != nil {
-			ExitWithError(err)
-		}
-
-		if status == 1 {
-			fmt.Printf("\nTransaction verification success: %s\n", hash)
-		} else {
-			ExitWithError(fmt.Sprintf("\nVerification failed: %s\n", hash))
-		}
-	},
+	if status == 1 {
+		fmt.Printf("\nTransaction verification success: %s\n", hash)
+	} else {
+		ExitWithError(fmt.Sprintf("\nVerification failed: %s\n", hash))
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(verifyEthereumTrxCmd)
-
-	verifyEthereumTrxCmd.PersistentFlags().String("file", "", "bridge config file")
-	verifyEthereumTrxCmd.PersistentFlags().String("log", "", "bridge log file")
-	verifyEthereumTrxCmd.PersistentFlags().String("hash", "", "hash of the ethereum transaction")
-
-	_ = verifyEthereumTrxCmd.MarkFlagRequired("hash")
-	_ = verifyEthereumTrxCmd.MarkFlagRequired("file")
+	rootCmd.AddCommand(
+		createBridgeCommand(
+			USE,
+			SHORT,
+			LONG,
+			ConfirmEthereumTransaction,
+		))
 }
--- cmd/bridge.go
+++ cmd/bridge.go
@@ -1,1 +1,71 @@
 package cmd
+
+import (
+	"github.com/0chain/gosdk/core/conf"
+	"github.com/0chain/gosdk/zcnbridge"
+	"github.com/spf13/cobra"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+type HashCommand func(string)
+
+func createBridgeCommand(use, short, long string, comm HashCommand) *cobra.Command {
+	var cobraCommand = &cobra.Command{
+		Use:   use,
+		Short: short,
+		Long:  long,
+		Args:  cobra.MinimumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			var (
+				logPath    = "logs"
+				configFile string
+				configDir  string
+				hash       string
+				err        error
+			)
+
+			fflags := cmd.Flags()
+
+			if !fflags.Changed("hash") {
+				ExitWithError("Error: hash flag is missing")
+			}
+			if !fflags.Changed("file") {
+				ExitWithError("Error: file flag is missing")
+			}
+
+			configFlag, err := fflags.GetString("file")
+			if err != nil {
+				ExitWithError(err)
+			}
+
+			hash, err = fflags.GetString("hash")
+			if err != nil {
+				ExitWithError(err)
+			}
+
+			configDir, configFile = filepath.Split(configFlag)
+			configFile = strings.TrimSuffix(configFile, path.Ext(configFlag))
+
+			clientConfig, _ := conf.GetClientConfig()
+			if clientConfig.EthereumNode == "" {
+				ExitWithError("ethereum_node_url must be setup in config")
+			}
+
+			bridge := zcnbridge.SetupBridge(configDir, configFile, false, logPath)
+			bridge.SetupEthereumWallet()
+
+			comm(hash)
+		},
+	}
+
+	cobraCommand.PersistentFlags().String("file", "", "bridge config file")
+	cobraCommand.PersistentFlags().String("log", "", "bridge log file")
+	cobraCommand.PersistentFlags().String("hash", "", "hash of the ethereum transaction")
+
+	_ = cobraCommand.MarkFlagRequired("hash")
+	_ = cobraCommand.MarkFlagRequired("file")
+
+	return cobraCommand
+}
