80ff14bf8a689e19d84da755471d81b0652a11c2
Piers Shepperson <piers.shepperson@btinternet.com>
2022-07-07 13:42:10 +0100

    update go.mod

---
+++ cmd/kill-miner.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/0chain/gosdk/zcncore"
+	"github.com/spf13/cobra"
+	"log"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var minerscKill = &cobra.Command{
+	Use:   "mn-kill",
+	Short: "kill miner",
+	Long:  "kill miner",
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		var (
+			flags = cmd.Flags()
+			id    string
+			err   error
+		)
+
+		if !flags.Changed("id") {
+			log.Fatal("missing id flag")
+		}
+
+		if id, err = flags.GetString("id"); err != nil {
+			log.Fatal(err)
+		}
+
+		var (
+			wg        sync.WaitGroup
+			statusBar = &ZCNStatus{wg: &wg}
+		)
+		txn, err := zcncore.NewTransaction(statusBar, 0, nonce)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wg.Add(1)
+		err = txn.MinerSCKillMiner(id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wg.Wait()
+
+		if !statusBar.success {
+			log.Fatal("fatal:", statusBar.errMsg)
+		}
+
+		statusBar.success = false
+		wg.Add(1)
+		if err = txn.Verify(); err != nil {
+			log.Fatal(err)
+		}
+		wg.Wait()
+
+		if statusBar.success {
+			switch txn.GetVerifyConfirmationStatus() {
+			case zcncore.ChargeableError:
+				ExitWithError("\n", strings.Trim(txn.GetVerifyOutput(), "\""))
+			case zcncore.Success:
+				fmt.Println("killed :", id)
+			default:
+				ExitWithError("\nkill " + id + " failed. Unknown status code: " +
+					strconv.Itoa(int(txn.GetVerifyConfirmationStatus())))
+			}
+			return
+		} else {
+			log.Fatal("fatal:", statusBar.errMsg)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(minerscKill)
+
+	minerscKill.PersistentFlags().Bool("json", false, "as JSON")
+	minerscKill.PersistentFlags().Bool("json", false, "as JSON")
+	minerscKill.PersistentFlags().Bool("all", false, "include all registered sharders")
+
+	minerscKill.PersistentFlags().String("id", "", "miner/sharder ID to update")
+	_ = minerscKill.MarkFlagRequired("id")
+
+}
