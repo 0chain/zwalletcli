e46ec0ca54ce387c1e4a6d2c0b1647a3c62b26b3
Bisakh Mondal <bisakhmondal00@gmail.com>
2022-09-27 23:10:46 +0100

    minor change

--- cmd/create-wallet.go
+++ cmd/create-wallet.go
@@ -11,11 +11,11 @@ var createWalletCmd = &cobra.Command{
 	Use:   "create-wallet",
 	Short: "Create wallet and logs it into stdout",
 	Long:  `Create wallet and logs it into standard output`,
-	Args:  cobra.MinimumNArgs(0),
+	Args:  cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		statusBar, err := createWallet()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create wallet: %v", err)
+			ExitWithError(fmt.Printf("Failed to create wallet: %v", err))
 		}
 		fmt.Fprintf(os.Stdout, "\n\t======WALLET_START======\n\n")
 		fmt.Fprintf(os.Stdout, statusBar.walletString)
