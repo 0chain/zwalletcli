5de7030b3bc65ea05e51742614b7e525cdb90f28
Bisakh Mondal <bisakhmondal00@gmail.com>
2022-10-28 21:25:33 +0100

    feat: support offline wallet creation in cli

--- cmd/create-wallet.go
+++ cmd/create-wallet.go
@@ -2,27 +2,59 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/0chain/gosdk/zcncore"
 	"github.com/spf13/cobra"
 )
 
 var createWalletCmd = &cobra.Command{
 	Use:   "create-wallet",
-	Short: "Create wallet and logs it into stdout",
-	Long:  `Create wallet and logs it into standard output`,
+	Short: "Create wallet and logs it into stdout (pass --register to register wallet to blockchain)",
+	Long:  `Create wallet and logs it into standard output (pass --register to register wallet to blockchain)`,
 	Args:  cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		statusBar, err := createWallet()
+
+		online, err := cmd.Flags().GetBool("register")
 		if err != nil {
-			ExitWithError(fmt.Printf("Failed to create wallet: %v", err))
+			ExitWithError("invalid register flag")
 		}
+
+		var walletStr string
+		if online {
+			statusBar, err := createWallet()
+			if err != nil {
+				ExitWithError(fmt.Printf("Failed to create wallet: %v", err))
+			}
+			walletStr = statusBar.walletString
+
+			// update gosdk wallet to use the newly generate wallet for following operation
+			if err = zcncore.SetWalletInfo(walletStr, false); err != nil {
+				ExitWithError("failed to use new wallet", err)
+			}
+
+			log.Print("Creating related read pool for storage smart-contract...")
+			if err := createReadPool(); err != nil {
+				log.Fatalf("Failed to create read pool: %v", err)
+			}
+			log.Printf("Read pool created successfully")
+
+			// Lock read pool ? TODO: clarification from @dabasov
+		} else {
+			walletStr, err = zcncore.CreateWalletOffline()
+			if err != nil {
+				ExitWithError("failed to generate offline wallet", err)
+			}
+		}
+
 		fmt.Fprintf(os.Stdout, "\n\t======WALLET_START======\n\n")
-		fmt.Fprintf(os.Stdout, statusBar.walletString)
+		fmt.Fprintf(os.Stdout, walletStr)
 		fmt.Fprintf(os.Stdout, "\n\n\t======WALLET_END======\n")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(WithoutWallet(createWalletCmd))
+	createWalletCmd.PersistentFlags().Bool("register", false, "create wallet with registration on blockchain")
 }
