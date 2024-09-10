7d596fed9e0f7d0a01d113f0324ef313172e9763
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-09-10 19:24:25 +0530

    Update

--- cmd/auth-register.go
+++ cmd/auth-register.go
@@ -1,110 +1,111 @@
 package cmd
 
-import (
-	"context"
-	"log"
-	"strings"
-
-	"github.com/0chain/gosdk/zcnbridge"
-	"github.com/0chain/gosdk/zcnbridge/transaction"
-	"github.com/0chain/gosdk/zcncore"
-	"github.com/pkg/errors"
-)
-
-//goland:noinspection ALL
-func init() {
-	rootCmd.AddCommand(
-		createCommandWithBridge(
-			"auth-register",
-			"Register an authorizer manually",
-			"Register an authorizer manually",
-			registerAuthorizerInChain,
-			true,
-			&Option{
-				name:     "url",
-				typename: "string",
-				value:    "",
-				usage:    "authorizer endpoint url",
-				required: true,
-			},
-			&Option{
-				name:     "client_id",
-				typename: "string",
-				value:    "",
-				usage:    "the client_id of the wallet",
-				required: true,
-			},
-			&Option{
-				name:     "client_key",
-				typename: "string",
-				value:    "",
-				usage:    "the client_key which is the public key of the wallet",
-				required: true,
-			},
-			&Option{
-				name:     "min_stake",
-				typename: "int64",
-				value:    int64(1),
-				usage:    "the minimum stake value for the stake pool",
-				required: false,
-			},
-			&Option{
-				name:     "max_stake",
-				typename: "int64",
-				value:    int64(10),
-				usage:    "the maximum stake value for the stake pool",
-				required: false,
-			},
-			&Option{
-				name:     "num_delegates",
-				typename: "int",
-				value:    5,
-				usage:    "the number of delegates in the authorizer stake pool",
-				required: false,
-			},
-			&Option{
-				name:     "service_charge",
-				typename: "float64",
-				value:    0.0,
-				usage:    "the service charge for the authorizer stake pool",
-				required: false,
-			},
-		))
-}
-
-// registerAuthorizerInChain registers a new authorizer
-// addAuthorizerPayload *addAuthorizerPayload
-func registerAuthorizerInChain(bc *zcnbridge.BridgeClient, args ...*Arg) {
-	clientID := GetClientID(args)
-	clientKey := GetClientKey(args)
-	url := GetURL(args)
-	numDelegates := GetNumDelegates(args)
-	serviceCharge := GetServiceCharge(args)
-
-	input := &zcncore.AddAuthorizerPayload{
-		PublicKey: clientKey,
-		URL:       url,
-		StakePoolSettings: zcncore.AuthorizerStakePoolSettings{
-			DelegateWallet: clientID,
-			NumDelegates:   numDelegates,
-			ServiceCharge:  serviceCharge,
-		},
-	}
-
-	trx, err := transaction.AddAuthorizer(context.Background(), input)
-	if err != nil {
-		log.Fatal(err, "failed to add authorizer with transaction: '%s'", trx.GetHash())
-	}
-
-	log.Printf("Authorizer submitted OK... " + trx.GetHash())
-	log.Printf("Starting verification: " + trx.GetHash())
-
-	err = trx.Verify(context.Background())
-	if err != nil {
-		if strings.Contains(err.Error(), "already exists") {
-			ExitWithError("Authorizer has already been added to 0Chain...  Continue")
-		} else {
-			ExitWithError(errors.Wrapf(err, "failed to verify transaction: '%s'", trx.GetHash()))
-		}
-	}
-}
+//
+//import (
+//	"context"
+//	"log"
+//	"strings"
+//
+//	"github.com/0chain/gosdk/zcnbridge"
+//	"github.com/0chain/gosdk/zcnbridge/transaction"
+//	"github.com/0chain/gosdk/zcncore"
+//	"github.com/pkg/errors"
+//)
+//
+////goland:noinspection ALL
+//func init() {
+//	rootCmd.AddCommand(
+//		createCommandWithBridge(
+//			"auth-register",
+//			"Register an authorizer manually",
+//			"Register an authorizer manually",
+//			registerAuthorizerInChain,
+//			true,
+//			&Option{
+//				name:     "url",
+//				typename: "string",
+//				value:    "",
+//				usage:    "authorizer endpoint url",
+//				required: true,
+//			},
+//			&Option{
+//				name:     "client_id",
+//				typename: "string",
+//				value:    "",
+//				usage:    "the client_id of the wallet",
+//				required: true,
+//			},
+//			&Option{
+//				name:     "client_key",
+//				typename: "string",
+//				value:    "",
+//				usage:    "the client_key which is the public key of the wallet",
+//				required: true,
+//			},
+//			&Option{
+//				name:     "min_stake",
+//				typename: "int64",
+//				value:    int64(1),
+//				usage:    "the minimum stake value for the stake pool",
+//				required: false,
+//			},
+//			&Option{
+//				name:     "max_stake",
+//				typename: "int64",
+//				value:    int64(10),
+//				usage:    "the maximum stake value for the stake pool",
+//				required: false,
+//			},
+//			&Option{
+//				name:     "num_delegates",
+//				typename: "int",
+//				value:    5,
+//				usage:    "the number of delegates in the authorizer stake pool",
+//				required: false,
+//			},
+//			&Option{
+//				name:     "service_charge",
+//				typename: "float64",
+//				value:    0.0,
+//				usage:    "the service charge for the authorizer stake pool",
+//				required: false,
+//			},
+//		))
+//}
+//
+//// registerAuthorizerInChain registers a new authorizer
+//// addAuthorizerPayload *addAuthorizerPayload
+//func registerAuthorizerInChain(bc *zcnbridge.BridgeClient, args ...*Arg) {
+//	clientID := GetClientID(args)
+//	clientKey := GetClientKey(args)
+//	url := GetURL(args)
+//	numDelegates := GetNumDelegates(args)
+//	serviceCharge := GetServiceCharge(args)
+//
+//	input := &zcncore.AddAuthorizerPayload{
+//		PublicKey: clientKey,
+//		URL:       url,
+//		StakePoolSettings: zcncore.AuthorizerStakePoolSettings{
+//			DelegateWallet: clientID,
+//			NumDelegates:   numDelegates,
+//			ServiceCharge:  serviceCharge,
+//		},
+//	}
+//
+//	trx, err := transaction.AddAuthorizer(context.Background(), input)
+//	if err != nil {
+//		log.Fatal(err, "failed to add authorizer with transaction: '%s'", trx.GetHash())
+//	}
+//
+//	log.Printf("Authorizer submitted OK... " + trx.GetHash())
+//	log.Printf("Starting verification: " + trx.GetHash())
+//
+//	err = trx.Verify(context.Background())
+//	if err != nil {
+//		if strings.Contains(err.Error(), "already exists") {
+//			ExitWithError("Authorizer has already been added to 0Chain...  Continue")
+//		} else {
+//			ExitWithError(errors.Wrapf(err, "failed to verify transaction: '%s'", trx.GetHash()))
+//		}
+//	}
+//}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/0chain/zwalletcli
 go 1.21
 
 require (
-	github.com/0chain/gosdk v1.17.7-0.20240910113441-24db157c8cef
+	github.com/0chain/gosdk v1.17.7-0.20240910135021-2f0771c28b3f
 	github.com/ethereum/go-ethereum v1.13.2
 	github.com/icza/bitio v1.1.0
 	github.com/olekukonko/tablewriter v0.0.5
